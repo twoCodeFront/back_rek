98b534bcd0d84d061270aa6ef99e8b5d1900cb08
Mateusz Jaracz <47606680+twoCodeFront@users.noreply.github.com>
2025-04-30 19:57:09 +0200

    dodanie pilnowanie dostępu do routingu

--- example-app/routes/api.php
+++ example-app/routes/api.php
@@ -2,35 +2,23 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\InvoiceController;
+use App\Http\Controllers\VatModelController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+// Publiczne: rejestracja, logowanie i odświeżanie
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/refresh', [AuthController::class, 'refresh']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::get('/user', fn(Request $r) => $r->user());
+    Route::apiResource('invoices', InvoiceController::class);
+    Route::apiResource('vat', VatModelController::class);
+    Route::apiResource('products', ProductController::class);
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
 
-Route::apiResource('invoices', InvoiceController::class);
-
-Route::apiResource('vat', \App\Http\Controllers\VatModelController::class);
-
-Route::apiResource('products', \App\Http\Controllers\ProductController::class);
-
-
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login',    [AuthController::class, 'login']);
-Route::post('/refresh',  [AuthController::class, 'refresh']);
-Route::post('/logout',   [AuthController::class, 'logout'])
-    ->middleware('auth:sanctum');
-
-Route::middleware('auth:sanctum')->get('/user', fn(Request $r) => $r->user());
